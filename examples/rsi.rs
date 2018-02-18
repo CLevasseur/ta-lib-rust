7cf43009dd8fd6b1ecc0040887f717067d07cf2a
Carl Levasseur <levasseur.cl@gmail.com>
2018-02-18 17:50:59 +0100

    Update rsi.rs

--- examples/rsi.rs
+++ examples/rsi.rs
@@ -23,7 +23,7 @@ fn rsi(period: u32, close_prices: &Vec<TA_Real>) -> (Vec<TA_Real>, TA_Integer) {
         match ret_code {
             // Indicator was computed correctly, since the vector was filled by TA-lib C library,
             // Rust doesn't know what is the new length of the vector, so we set it manually
-            // to the number of values returned by the TA_MA call
+            // to the number of values returned by the TA_RSI call
             TA_RetCode::TA_SUCCESS => out.set_len(out_size as usize),        
             // An error occured
             _ => panic!("Could not compute indicator, err: {:?}", ret_code)  
