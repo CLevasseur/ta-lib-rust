b5bff5a2e77089658442f04887762a1fa435b90c
Carl Levasseur <carl.levasseur@protonmail.com>
2018-02-18 17:37:20 +0100

    add rsi example

--- examples/rsi.rs
+++ examples/rsi.rs
@@ -1,23 +1,22 @@
 extern crate ta_lib_wrapper;
-use ta_lib_wrapper::{TA_Integer, TA_Real, TA_MA, TA_MAType, TA_RetCode};
+use ta_lib_wrapper::{TA_Integer, TA_Real, TA_RSI,  TA_RetCode};
 
 
-/// Compute SMA(period) on `close_prices`
-/// This function returns a tuple containing the list of sma values and the index of the first
-/// close to have an associated sma value
-fn sma(period: u32, close_prices: &Vec<TA_Real>) -> (Vec<TA_Real>, TA_Integer) {
+/// Compute RSI(period) on `close_prices`
+/// This function returns a tuple containing the list of rsi values and the index of the first
+/// close to have an associated rsi value
+fn rsi(period: u32, close_prices: &Vec<TA_Real>) -> (Vec<TA_Real>, TA_Integer) {
     let mut out: Vec<TA_Real> = Vec::with_capacity(close_prices.len());
     let mut out_begin: TA_Integer = 0;
     let mut out_size: TA_Integer = 0;
 
     unsafe {
-        let ret_code = TA_MA(
+        let ret_code = TA_RSI(
             0,                              // index of the first close to use
             close_prices.len() as i32 - 1,  // index of the last close to use
             close_prices.as_ptr(),          // pointer to the first element of the vector
-            period as i32,                  // period of the sma
-            TA_MAType::TA_MAType_SMA,       // type of the MA, here forced to sma
-            &mut out_begin,                 // set to index of the first close to have an sma value
+            period as i32,                  // period of the rsi
+            &mut out_begin,                 // set to index of the first close to have an rsi value
             &mut out_size,                  // set to number of sma values computed
             out.as_mut_ptr()                // pointer to the first element of the output vector
         );
@@ -41,13 +40,11 @@ fn main() {
         1.086670, 1.086630
     ];
 
-    // compute sma, since we use a period of 10, the first 10 closes won't have
-    // an sma value because there is not enough data, so begin will be set to
-    // the index 29
-    let (sma_values, begin) = sma(10, &close_prices);
+    let (rsi_values, begin) = rsi(7, &close_prices);
 
     // print values
-    for (index, value) in sma_values.iter().enumerate() {
+    for (index, value) in rsi_values.iter().enumerate() {
         println!("Close index {} = {}", begin + index as i32 + 1, value);
     }
 }
+
